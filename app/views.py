de4360cee0ed727db1c3a7f943a90197eacb3ed5
amritarane <raneamrita123@gmail.com>
2023-07-31 12:02:28 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def data_render(request):
-    d={'name':'amrita','age':22}
+    d={'name':'amrita','age':22,'hobbies':['dancing','gardening']}
     return render(request,'data_render.html',context=d)
 def conditions(request):
     m={'a':20,'b':90,'c':10}
     return render(request,'conditions.html',context=m)
+
+def loops(request):
+    d={'name':'amrita','age':22,'hobbies':['dancing','gardening']}
+    return render(request,'loops.html',context=d)
